9177fbfbba2b088ad01206735a05a45463d6878f
unknown <22444128+jppaled@users.noreply.github.com>
2019-11-11 04:48:44 +0100

    Updates

---
+++ precache-manifest.5cf728f8c52c8a15d467eb402d373e6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759ee79876dbe9908872a08d63564987",
+    "url": "/youtube-playlist/index.html"
+  },
+  {
+    "revision": "3cc0ec2ed6065bd20075",
+    "url": "/youtube-playlist/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c965e1823ea06b91952c",
+    "url": "/youtube-playlist/static/js/2.335f9a31.chunk.js"
+  },
+  {
+    "revision": "3cc0ec2ed6065bd20075",
+    "url": "/youtube-playlist/static/js/main.88b71955.chunk.js"
+  },
+  {
+    "revision": "f9b9de9c4f60d03f4a45",
+    "url": "/youtube-playlist/static/js/runtime-main.8fe5175d.js"
+  }
+]);
