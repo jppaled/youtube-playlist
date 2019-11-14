b00371a822e7022fc4e038cbabbe1202414176ef
jppaled <22444128+jppaled@users.noreply.github.com>
2019-11-14 14:17:45 +0100

    Update Notification.js

--- src/Playlist/Components/Notification.js
+++ src/Playlist/Components/Notification.js
@@ -9,31 +9,31 @@ const variantIcon = {
     warning: Warning,
     error: ErrorIcon,
     info: Info,
-  };
-  
-  const useStyles1 = makeStyles(theme => ({
+};
+
+const useStyles1 = makeStyles(theme => ({
     success: {
-      backgroundColor: green[600],
+        backgroundColor: green[600],
     },
     error: {
-      backgroundColor: theme.palette.error.dark,
+        backgroundColor: theme.palette.error.dark,
     },
     info: {
-      backgroundColor: theme.palette.primary.main,
+        backgroundColor: theme.palette.primary.main,
     },
     warning: {
-      backgroundColor: amber[700],
+        backgroundColor: amber[700],
     },
     icon: {
-      fontSize: 20,
+        fontSize: 20,
     },
     iconVariant: {
-      opacity: 0.9,
-      marginRight: theme.spacing(1),
+        opacity: 0.9,
+        marginRight: theme.spacing(1),
     },
     message: {
-      display: 'flex',
-      alignItems: 'center',
+        display: 'flex',
+        alignItems: 'center',
     },
 }));
 
@@ -41,26 +41,26 @@ function MySnackbarContentWrapper(props) {
     const classes = useStyles1();
     const { className, message, onClose, variant, ...other } = props;
     const Icon = variantIcon[variant];
-  
+
     return (
-      <SnackbarContent
-        className={clsx(classes[variant], className)}
-        aria-describedby="client-snackbar"
-        message={
-          <span id="client-snackbar" className={classes.message}>
-            <Icon className={clsx(classes.icon, classes.iconVariant)} />
-            {message}
-          </span>
-        }
-        action={[
-          <IconButton key="close" aria-label="close" color="inherit" onClick={onClose}>
-            <Close className={classes.icon} />
-          </IconButton>,
-        ]}
-        {...other}
-      />
+        <SnackbarContent
+            className={clsx(classes[variant], className)}
+            aria-describedby="client-snackbar"
+            message={
+                <span id="client-snackbar" className={classes.message}>
+                    <Icon className={clsx(classes.icon, classes.iconVariant)} />
+                    {message}
+                </span>
+            }
+            action={[
+                <IconButton key="close" aria-label="close" color="inherit" onClick={onClose}>
+                    <Close className={classes.icon} />
+                </IconButton>,
+            ]}
+            {...other}
+        />
     );
-  }
+}
 
 function Notification(props) {
     return (
@@ -78,7 +78,7 @@ function Notification(props) {
                 variant={props.notification.color}
                 message={props.notification.message}
             />
-        </Snackbar>    
+        </Snackbar>
     );
 }
 
