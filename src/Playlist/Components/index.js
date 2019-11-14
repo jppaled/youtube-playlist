9ba8966d48ab4ee7f94e5b9d81c11540778f3433
jppaled <22444128+jppaled@users.noreply.github.com>
2019-11-14 16:03:20 +0100

    Update index.js

--- src/Playlist/Components/index.js
+++ src/Playlist/Components/index.js
@@ -1,6 +1,5 @@
 export { default as Clear } from './Clear';
 export { default as Form } from './Form';
-export { default as Header } from './Header';
 export { default as Import } from './Import';
 export { default as Notification } from './Notification';
 export { default as Url } from './Url';
