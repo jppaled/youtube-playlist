455ab7fbc2fead41b5e411faf56a2b58b5ec2251
jppaled <22444128+jppaled@users.noreply.github.com>
2019-11-14 14:14:05 +0100

    add header and searchBar components

---
+++ src/Search/Components/SearchBar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { CircularProgress, Divider, IconButton, InputBase, makeStyles, Paper } from '@material-ui/core';
+import { Search as SearchIcon } from '@material-ui/icons';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: '2px 15px',
+        display: 'flex',
+        alignItems: 'center',
+        width: 400,
+        borderRadius: '25px',
+        marginBottom: '25px'
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
+    },
+    iconButton: {
+        padding: 10,
+    },
+    divider: {
+        height: 28,
+        margin: 4,
+    },
+}));
+
+function SearchBar(props) {
+    const classes = useStyles();
+    const [searchValue, setSearchValue] = React.useState('');
+    const [loading, setLoading] = React.useState(false);
+
+    const handleSubmit = (event) => {
+        setLoading(true);
+        event.preventDefault();
+        props.handleSubmit(searchValue, () => {
+            setLoading(false);
+        });
+    }
+
+    return (
+        <Paper 
+            component="form" 
+            className={classes.root} 
+            onSubmit={handleSubmit}
+        >
+            <InputBase
+                className={classes.input}
+                placeholder="Search Youtube video"
+                inputProps={{ 'aria-label': 'search youtube video' }}
+                onChange={(event) => setSearchValue(event.target.value)}
+            />
+            <Divider className={classes.divider} orientation="vertical" />
+            {loading ? (
+                 <CircularProgress size={30} />
+            ) : (
+                <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                    <SearchIcon />
+                </IconButton>
+            )}
+        </Paper>
+    );
+}
+
+export default SearchBar;
