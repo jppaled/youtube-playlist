59a76f61b509516558a70c87ffd2d6a098a741ab
jppaled <22444128+jppaled@users.noreply.github.com>
2019-11-14 15:45:46 +0100

    Update Search.js

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -4,56 +4,37 @@ import { Header, SearchBar } from './Components';
 import { List } from '../Components'
 import { formatVideosInfos, getVideosBySearch } from '../Utils/youtube';
 
-class Search extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            searchResult: [],
-        };
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSubmit = async (search, callBack) => {
+export default function Search(props) {
+    const [searchResult, setsearchResult] = React.useState([])
+     
+    const handleSubmit = async (search, callBack) => {
         let videos = await getVideosBySearch(search);
         let res = await formatVideosInfos(videos);
 
-        this.setState({ searchResult: res })
+        setsearchResult(res);
 
         callBack();
     }
-
-    render() {
-        const { searchResult } = this.state;
-        
-        return (
-            <Grid container direction="column">
-                <Grid item>
-                    <Grid container>
-                        <Grid item>
-                            <Header />
-                        </Grid>
+       
+    return (
+        <Grid container direction="column">
+            <Grid item>
+                <Grid container>
+                    <Grid item>
+                        <Header />
                     </Grid>
-                    <Grid container justify="center">
-                        <Grid item>
-                            <SearchBar 
-                                handleSubmit={this.handleSubmit}
-                            />
-                        </Grid>
+                </Grid>
+                <Grid container justify="center">
+                    <Grid item>
+                        <SearchBar handleSubmit={handleSubmit} />
                     </Grid>
-                    <Grid container>
-                        <Grid item>
-                            <List 
-                                playlist={searchResult} 
-                                justify="center"
-                            />
-                        </Grid>
+                </Grid>
+                <Grid container>
+                    <Grid item>
+                        <List playlist={searchResult} justify="center" />
                     </Grid>
                 </Grid>
             </Grid>
-        );
-    }
+        </Grid>
+    );
 }
-
-export default Search;
