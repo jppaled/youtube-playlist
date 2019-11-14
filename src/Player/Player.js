761fd15a4e21279f98d3022247c2e695f1e2ff70
jppaled <22444128+jppaled@users.noreply.github.com>
2019-11-14 17:16:44 +0100

    Create Player.js

---
+++ src/Player/Player.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
+import { Grid, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        position: "relative",
+        width: "100%",
+        height: "0",
+        paddingBottom: "55.2%"
+    },
+    video: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+    }
+}));
+
+export default function Player(props) {
+    const classes = useStyles();
+    let { id } = useParams();
+
+    return (
+        <Grid container justify="center" className={classes.container}>
+            <Grid item>
+                <iframe 
+                    className={classes.video}
+                    src={`https://www.youtube.com/embed/${id}`}
+                    frameborder="0" 
+                    allow="accelerometer; 
+                    autoplay; 
+                    encrypted-media; 
+                    gyroscope; 
+                    picture-in-picture" 
+                >
+                </iframe>
+            </Grid>
+        </Grid>
+    );
+}
